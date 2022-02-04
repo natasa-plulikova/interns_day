ee5bc877d98eb82c34d5c3efe20e2016cb9afcff
Otto Novak <ottonovak16@gmail.com>
2022-02-04 17:21:59 +0100

    Final Commit, corellation

--- main.py
+++ main.py
@@ -1,60 +1,13 @@
 # Project of Stefan Otto Novak
-from csv import reader
-List_Recordings = []
-REQUESTS = {}
+
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-
-class Record:
-    def __init__(self, id, timestamp, number_of_requests, number_of_errors, response_time, cpu_cores, memory_usage, cpu_usage):
-        self.id = id
-        self.timestamp = timestamp
-        self.number_of_requests = number_of_requests
-        self.number_of_errors = number_of_errors
-        self.response_time = response_time
-        self.cpu_cores = cpu_cores
-        self.memory_usage = memory_usage
-        self.cpu_usage = cpu_usage
+data = pd.read_csv('train.csv')
+data.head()
 
 
-# Load the data
-with open('data/train.csv', 'r') as file:    # skip the first line(the header)
-    file_csv = reader(file)
-    head = next(file_csv)
-
-    if head is not None:        # check if the file is empty or not
-        for i in file_csv:      # Iterate over each row
-            a_data = str(i).split(", ")
-
-            id = a_data[0]
-            timestamp = a_data[1]
-            number_of_requests = a_data[2]
-            number_of_errors = a_data[3]
-            response_time = a_data[4]
-            cpu_cores = a_data[5]
-            memory_usage = a_data[6]
-            cpu_usage = a_data[7]
-
-            new_record = Record(id, timestamp, number_of_requests, number_of_errors, response_time, cpu_cores, memory_usage, cpu_usage)
-            List_Recordings.append(new_record)
-
-
-
-# At what time are most requests
-for record in List_Recordings:
-    hour = int(record.timestamp[12:14])
-    #print(hour)
-    if REQUESTS.__contains__(hour):
-        REQUESTS[hour] += 1
-    else:
-        REQUESTS[hour] = 1
-
-print(str(REQUESTS[13]) + " requets at hour 17")
-
-
-
-
-
-
-
-
+plt.figure(figsize=(36, 15))
+plt.scatter(data['response_time'], data['cpu_usage'], c= '#DDBEA9')
+plt.show()
